8a8ed90fa2ef3b0de037d68d0d599d13f934e492
Lachlan Connelly <lachlan.c.95@gmail.com>
2020-01-09 11:29:20 +1100

    Commented in the new javascript file

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function getQuote() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  var xhttp = new XMLHttpRequest(); //Setup ajax object
+  xhttp.onreadystatechange = function() { //Function to update the html with the response
 	if (this.readyState == 4 && this.status == 200) {
 	  document.getElementById("content").innerHTML = this.responseText;
 	}
   };
-  xhttp.open("GET", "quote.php", true);
+  xhttp.open("GET", "quote.php", true); //Get the php page output that talks to the sql server
   xhttp.send();
 }
